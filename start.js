6598be15dbe03b2a973bc079074064b9e05f2093
Denny <1160345031@qq.com>
2021-08-18 23:40:47 +0800

    cli publish

--- start.js
+++ start.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
+const { program } = require("commander");
 
-console.log('Hello world!');
+// 定义当前版本
+program
+  .version(
+    require("./package.json").version,
+    "-v, --vers",
+    "output the current version"
+  );
+
+program.usage("<command>");
+program.parse(process.argv);
